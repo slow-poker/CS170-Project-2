5393c172e939fc784c60fe1b5f88596326387f44
slow-poker <46554183+slow-poker@users.noreply.github.com>
2024-12-09 20:43:48 -0800

    completed classifier train()

--- classifier.py
+++ classifier.py
@@ -1,29 +1,42 @@
+import copy
 
 class Classifier:
     def __init__(self):
-        self._featureSet = [[]]
+        self._featureSet = []
+        self._normFeatureSet = []
+        self._classSet = []
 
-    @property
-    def featureSet(self):
-        return self._featureSet
-
+    @staticmethod
+    def normalizeData(dataSet):
+        normalizedSet = []
+        transposed = [[row[i] for row in dataSet] for i in range(len(dataSet[0]))]
+        for featureCol in transposed:
+            tempList = []
+            dataMax = max(featureCol)
+            dataMin = min(featureCol)
+            for data in featureCol:
+                newData = (data-dataMin)/(dataMax-dataMin)
+                tempList.append(newData)
+            normalizedSet.append(tempList)
+        return normalizedSet           
+            
     def train(self, fileName): #input training data into vectors
         #https://www.geeksforgeeks.org/python-program-to-read-file-word-by-word/
         with open(fileName, 'r') as file:
             for line in file:
                 strToFloatList = [float(item) for item in line.split()]
-                self._featureSet.append(strToFloatList)
-        self._featureSet = list(filter(None, self._featureSet))       
-        
-        # #test
-        # num=1
-        # for i in self._featureSet:
-        #     print(i)
-        #     print(str(num))
-        #     num += 1
+                self._classSet.append(strToFloatList[0])
+                self._featureSet.append(strToFloatList[1:])
+        self._featureSet = list(filter(None, self._featureSet))     
+        self._classSet = list(filter(None, self._classSet))    
+        self._normFeatureSet = copy.deepcopy(self.normalizeData(self._featureSet))
 
-    def test(): #
-        pass
+
+    def test(self, testPoint): #find distance to all points, return label of closest
+        closestPoint = 0
+        for allPoints in self.featureSet:
+            if(closestPoint > self.distance(allPoints, testPoint)):
+                pass
 
     @staticmethod
     def distance(startPoint, endPoint): #returns euclidean distance between two points
